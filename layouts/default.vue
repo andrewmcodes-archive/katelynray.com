0b033b6081e6458ff191864228f1fa5ba993b39e
Andrew Mason <andrew.mason@n2pub.com>
2018-12-12 20:10:59 -0500

    update colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,32 +24,19 @@ html {
   margin: 0;
 }
 
-.button--green {
+.button--purple {
   display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+
   text-decoration: none;
   padding: 10px 30px;
-}
-
-.button--green:hover {
   color: #fff;
-  background-color: #3b8070;
+  background-color: #794acf;
 }
 
-.button--grey {
-  display: inline-block;
+.button--purple:hover {
   border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+  border: 1px solid #794acf;
+  color: #794acf;
+  background-color: #fff;
 }
 </style>
